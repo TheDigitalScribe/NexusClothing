40acfde2269b69a9048de56a43d8da6defa9c511
TheHaloEffect <githubemail165@gmail.com>
2024-10-31 11:02:19 +0000

    remove Jest tests to redo later, downgrade eslint for compatibility

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -3,11 +3,10 @@ import React from 'react'
 interface Button {
   text: string;
   ariaLabel: string;
-  onClick: () => void;
 }
 
-export const Button = ({ text, ariaLabel, onClick }: Button) => {
+export const Button = ({ text, ariaLabel }: Button) => {
   return (
-    <button onClick={onClick} aria-label={ariaLabel} className="rounded-full py-2 px-4 bg-blue-600 items-center text-white shadow-sm transition-colors duration-300">{text}</button>
+    <button aria-label={ariaLabel} className="rounded-full py-2 px-4 bg-blue-600 items-center text-white shadow-sm transition-colors duration-300">{text}</button>
   )
 }
